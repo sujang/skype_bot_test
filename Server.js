11bc3d08a1879fad0b291da0053976e5fd30553b
kangsujang <kangsujang@a-i-s.co.jp>
2016-05-17 21:06:58 +0900

    aaa

--- Server.js
+++ Server.js
@@ -13,10 +13,12 @@ const botService = new skype.BotService({
 });
 
 botService.on('contactAdded', (bot, data) => {
+	console.log('contactAdded comes');
     bot.reply(`Hello ${data.fromDisplayName}!`, true);
 });
 
 botService.on('personalMessage', (bot, data) => {
+	console.log('personalMessage comes');
     bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
 });
 
